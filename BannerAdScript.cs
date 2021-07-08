ea0d0743ceeebba5a32f7790e4661e5a27c3a083
Danilo Filitto <danilo.filitto@gmail.com>
2021-07-08 13:54:26 -0300

    ads

---
+++ BannerAdScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAdScript : MonoBehaviour
+{
+	//código do jogo no Android ou IOS
+    string gameId = "4110283";
+	//código da propaganda
+    string myPlacementId = "BannerADS";
+	//se a propaganda esta em teste ou não
+    bool testMode = false;
+
+    void Start()
+    {
+		//Inicia a propaganda
+        Advertisement.Initialize(gameId, testMode);
+        //definir local do banner TOP_LEFT, TOP_CENTER, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT, CENTER
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        StartCoroutine(ShowBannerWhenInitialized());
+    }
+
+    IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(myPlacementId);
+    }
+}
---
+++ InterstitialAdsScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InterstitialAdsScript
+{
+
+    public string GameId { get; set; }
+    public string MyPlacementId { get; set; }
+    public bool TestMode { get; set; }
+
+    //construtor com parâmetros já definidos
+    //public InterstitialAdsScript()
+    //{
+    //    //O ideal é não deixar pré definido esses valores
+    //    //o ideal é utilizar o outro construtor
+    //    this.GameId = "4110283";
+    //    this.MyPlacementId = "VideoADS";
+    //    this.TestMode = true;
+    //    Advertisement.Initialize(GameId, TestMode);
+    //}
+
+    public InterstitialAdsScript(string gameId, string myPlacementId, bool testMode)
+    {
+        
+        this.GameId = gameId;
+        this.MyPlacementId = myPlacementId;
+        this.TestMode = testMode;
+        Advertisement.Initialize(GameId, TestMode);
+    }
+    public void ShowInterstitialAd()
+    {
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show(MyPlacementId);
+        }
+        else
+        {
+            Debug.Log("Interstitial ad not ready at the moment! Please try again later!");
+        }
+    }
+}
